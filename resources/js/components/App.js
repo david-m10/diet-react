145b727c469dd897e025bdbf592961f5ba3813ad
david-m10 <musialik.przemek@gmail.com>
2019-01-06 00:24:47 +0100

    Experiment with custom palette color

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -4,17 +4,27 @@ import Header from './Header'
 import DishesList from "./Dishes/DishesIndex";
 import DishesCreate from "./Dishes/DishesCreate";
 import DishesShow from "./Dishes/DishesShow";
+import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
+import blueGrey from '@material-ui/core/colors/blueGrey';
+
+const theme = createMuiTheme({
+    palette: {
+        primary: blueGrey,
+    },
+});
 
 export default class App extends Component {
     render() {
         return (
             <div>
-                <Header/>
-                <Switch>
-                    <Route exact path='/dishes/create' component={DishesCreate}/>
-                    <Route path='/dishes/:id([0-9]+)' component={DishesShow}/>
-                    <Route exact path='/dishes/:urlData([a-zA-Z0-9/+_,=,]+)?' component={DishesList}/>
-                </Switch>
+                <MuiThemeProvider theme={theme}>
+                    <Header/>
+                    <Switch>
+                        <Route exact path='/dishes/create' component={DishesCreate}/>
+                        <Route path='/dishes/:id([0-9]+)' component={DishesShow}/>
+                        <Route exact path='/dishes/:urlData([a-zA-Z0-9/+_,=,]+)?' component={DishesList}/>
+                    </Switch>
+                </MuiThemeProvider>
             </div>
         )
     }
