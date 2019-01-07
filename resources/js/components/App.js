aa0285cd2e3a13c5a546cb9bcafc5a53f99b9a7a
david-m10 <musialik.przemek@gmail.com>
2019-01-08 00:59:51 +0100

    Fix typo

--- resources/js/components/App.js
+++ resources/js/components/App.js
@@ -1,11 +1,11 @@
 import React, {Component} from 'react';
 import {Route, Switch} from 'react-router-dom'
-import Header from './Header'
-import DishesList from "./Dishes/DishesIndex";
-import DishesCreate from "./Dishes/DishesCreate";
-import DishesShow from "./Dishes/DishesShow";
 import {MuiThemeProvider, createMuiTheme} from '@material-ui/core/styles';
 import blueGrey from '@material-ui/core/colors/blueGrey';
+import Header from './Header'
+import DishesIndex from "./Dishes/DishesIndex";
+import DishesCreate from "./Dishes/DishesCreate";
+import DishesShow from "./Dishes/DishesShow";
 
 const theme = createMuiTheme({
     palette: {
@@ -22,7 +22,7 @@ export default class App extends Component {
                     <Switch>
                         <Route exact path='/dishes/create' component={DishesCreate}/>
                         <Route path='/dishes/:id([0-9]+)' component={DishesShow}/>
-                        <Route exact path='/dishes/:urlData([a-zA-Z0-9/+_,=,]+)?' component={DishesList}/>
+                        <Route exact path='/dishes/:urlData([a-zA-Z0-9/+_,=,]+)?' component={DishesIndex}/>
                     </Switch>
                 </MuiThemeProvider>
             </div>
